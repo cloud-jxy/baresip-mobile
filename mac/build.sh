8557a713283aa62c14766b04eb0ea893f48aacbb
jiangxueyun <jiangxueyun@x-y-t.cn>
2016-07-14 10:26:47 +0800

    add mac

---
+++ mac/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cp ../package/*.tar.gz ./
+
+tar -zxvf re-0.4.17.tar.gz && mv re-0.4.17 re
+tar -zxvf rem-0.4.7.tar.gz && mv rem-0.4.7 rem
+tar -zxvf baresip-0.4.19.tar.gz && mv baresip-0.4.19 baresip
+
+rm *.tar.gz
+
+cd re && make && sudo make install && cd ../
+cd rem && make && sudo make install && cd ../
+cd baresip && make && sudo make install && cd ../
+
+
+#make contrib
